505fffaa409833e2dda8bdb483fedac4190f0fdc
ursusma <736324424@qq.com>
2016-12-27 00:27:45 +0800

    add login function

--- WebMM/RedisWebapp/views.py
+++ WebMM/RedisWebapp/views.py
@@ -1,3 +1,85 @@
-from django.shortcuts import render
+# coding=utf-8
+from django.shortcuts import render,render_to_response,redirect
+from django.http import HttpResponse,HttpResponseRedirect
+from .vfication import *
 
-# Create your views here.
+import hashlib
+
+setuser = vf()
+
+def regist(request):
+    error = False
+    if request.method == 'POST':
+#        if request.POST['username'] and request.POST['password'] and request.POST['name']:
+        if request.POST['password'] != request.POST['password_1']:
+            data = 'The two passwords are inconsistent!'
+        else:
+            username = request.POST['username']
+            password = request.POST['password']
+            name = request.POST['name']
+            val = setuser.get_cache(username)
+            if type(val) == str:
+                data = 'Username already exists!'
+            else:
+                if val['name'] == name.encode('utf-8'):
+                    data = 'Name already exists!'
+                else:
+                    pwd = hashlib.sha1(password.encode('utf-8')).hexdigest()
+                    setuser.set_cache(username,name,pwd)
+                    return render(request,'RedisWebapp/login.html')
+        error = True
+        return render(request,'RedisWebapp/sign.html',{'error':error,'data':data})
+    return render(request,'RedisWebapp/sign.html')
+
+def login(request):
+    error = False
+    if 'username' in request.session:
+        if 'time' in request.session:
+            del request.session['time']
+        del request.session['username']
+    if request.method == 'POST':
+        if 'username' in request.POST and 'password' in request.POST:
+#        if request.POST['username'] and request.POST['password']:
+                    #response = HttpResponseRedirect('index')
+                    #response.set_cookie("username",'admin',3600)
+                    #return response
+            username = request.POST['username']
+            password = request.POST['password']
+            val = setuser.get_cache(username)
+            if type(val) == dict:
+#                return render(request,'RedisWebapp/login.html') 
+#            else:
+                pwd = hashlib.sha1(password.encode('utf-8')).hexdigest()
+                if pwd.encode('utf-8') == val['pwd']:
+                    time = val.get('time','')
+                    if time == '':
+                        request.session['username'] = val['name'].decode('utf-8')
+                    else:
+                        request.session['username'] = val['name'].decode('utf-8')
+                        request.session['time'] = val['time'].decode('utf-8')
+                    return HttpResponseRedirect('index')
+        error = True
+        return render(request,'RedisWebapp/login.html',{'error':error})
+#    else:
+    return render(request,'RedisWebapp/login.html',{'error':error})
+
+def index(request):
+    #username = request.COOKIES.get('username','')
+    if 'username' in request.session:
+        if 'time' in request.session:
+            time = request.session.get('time')
+            username = request.session.get('username')
+            return render(request,'RedisWebapp/index.html',{'username':username,'time':time})
+        else:
+            username = request.session.get('username')
+            return render(request,'RedisWebapp/index.html',{'username':username})
+    return render(request,'RedisWebapp/login.html')
+
+#def logout(request):
+#    if request.session['time']:
+#        del request.session['time']
+    #response = HttpResponse('logout')
+    #response.delete_cookie('username')
+    #return response
+#    del request.session['username']
+#    return render(request,'RedisWebapp/login.html')
--- WebMM/mysite/settings.py
+++ WebMM/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'RedisWebapp',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -118,4 +119,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/RedisWebapp/static/'
