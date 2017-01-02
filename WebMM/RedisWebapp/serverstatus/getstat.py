7e3072e3b021c15edba9b99e05fa0b6f1c1d211e
ursusma <736324424@qq.com>
2017-01-03 00:42:58 +0800

    mysql error

--- WebMM/RedisWebapp/serverstatus/getstat.py
+++ WebMM/RedisWebapp/serverstatus/getstat.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from ..mysqlcache.conmysql import *
 import os
-import datetime
-
-cmy = cmysql()
 
 class gsta(object):
 
@@ -17,17 +13,3 @@ class gsta(object):
         d['free'] = round(int(d['free']) * 100 / int(d['total']),1)
         d['used'] = round(int(d['used']) * 100 / int(d['total']),1)
         return d
-
-    def getidle(self):
-        idle = os.popen('vmstat')
-        idledata = idle.read().split()
-        idledata = idledata[-3]
-        time = datetime.datetime.now().strftime("%H")
-        results = cmy.selidletdb()
-        if time in results:
-            cmy.delidletdb(time)
-        cmy.inidletdb(time,idledata)
-
-if __name__=='__main__':
-    b = gsta()
-    b.getidle()
